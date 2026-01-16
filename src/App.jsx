28ff6be837d8da5bc413f707f31a009ee9c6a184
InulMarliya <imarliya88@gmail.com>
2026-01-16 16:22:06 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import "./index.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PatientDashboard from "./pages/PatientDashboard";
